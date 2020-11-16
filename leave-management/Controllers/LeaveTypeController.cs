74a946618a46a2c50d9aac3e0e5c1967fc71313f
Baba Dorin <bvd.dorin@gmail.com>
2020-11-16 23:28:09 +0200

    Edit and Delete frunctionalities have been added - LeaveTypeController

--- leave-management/Controllers/LeaveTypeController.cs
+++ leave-management/Controllers/LeaveTypeController.cs
@@ -33,7 +33,14 @@ namespace leave_management.Controllers
         // GET: LeaveType/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeVM>(leaveType);
+            return View(model);
         }
 
         // GET: LeaveType/Create
@@ -104,31 +111,31 @@ namespace leave_management.Controllers
                 {
                     ModelState.AddModelError("", "Something went wrong...");
                     return View(model);
-                    // Here
+                    
                 }
 
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                ModelState.AddModelError("", "Something went wrong...");
+                return View(model);
             }
         }
 
         // GET: LeaveType/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
-        }
-
-        // POST: LeaveType/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
             try
             {
                 // TODO: Add delete logic here
+                var leaveType = _repo.FindById(id);
+                if (leaveType == null)
+                    return NotFound();
+
+                var isSuccess = _repo.Delete(leaveType);
+                if (!isSuccess)
+                    return BadRequest();
 
                 return RedirectToAction(nameof(Index));
             }
@@ -136,6 +143,23 @@ namespace leave_management.Controllers
             {
                 return View();
             }
+        }
+
+        // POST: LeaveType/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection, LeaveTypeVM model)
+        {
+            // TODO: Add delete logic here
+            var leaveType = _repo.FindById(id);
+            if (leaveType == null)
+                return NotFound();
+
+            var isSuccess = _repo.Delete(leaveType);
+            if (!isSuccess)
+                return View(model);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
