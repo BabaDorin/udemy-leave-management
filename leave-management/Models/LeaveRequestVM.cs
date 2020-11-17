693d7e58b2cedf2bcf89836b53a69a31226fd8b9
Baba Dorin <bvd.dorin@gmail.com>
2020-11-17 22:27:09 +0200

    Adding some statistics to LeaveRequest index page

--- leave-management/Models/LeaveRequestVM.cs
+++ leave-management/Models/LeaveRequestVM.cs
@@ -2,6 +2,7 @@
 using leave_management.Data.Migrations;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,10 +29,18 @@ namespace leave_management.Models
 
     public class AdminLeaveRequestVM
     {
+        [Display(Name = "Total Requests")]
         public int TotalRequests { get; set; }
+
+        [Display(Name = "Approved Requests")]
         public int ApprovedRequests { get; set; }
-        public int PendingRequests { get; set; }
+
+        [Display(Name = "Rejected Requests")]
         public int RejectedRequests { get; set; }
+
+        [Display(Name = "Pending Requests")]
+        public int PendingRequests { get; set; }
+       
         public List<LeaveRequestVM> LeaveRequests { get; set; }
     }
 }
